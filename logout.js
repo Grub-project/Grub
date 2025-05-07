e270bd9512f16298b08316cab8cc73a3029f1760
Grub-project <carmenjovani10@gmail.com>
2025-05-06 23:41:43 -0400

    Delete logout.js

--- logout.js
+++
@@ -1,9 +0,0 @@
-// logout.js
-import { supabase } from './supabaseClient.js';
-
-(async () => {
-  // Sign the user out
-  const { error } = await supabase.auth.signOut();
-  // Then redirect to home (or login)
-  window.location.href = 'index.html';
-})();
